6624afa48b4a586011705226e50a7cf9cf4bae73
katafuchix <katafuchix@gmail.com>
2026-04-29 14:32:24 +0900

    Initial Commit

---
+++ SwiftUI_TCA_ReqRes_API_Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_TCA_ReqRes_API_Example
+//
+//  Created by cano on 2026/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_TCA_ReqRes_API_Example/SwiftUI_TCA_ReqRes_API_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_ReqRes_API_ExampleApp.swift
+//  SwiftUI_TCA_ReqRes_API_Example
+//
+//  Created by cano on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_ReqRes_API_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
